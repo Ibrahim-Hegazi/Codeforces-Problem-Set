109f180089e212f08de23d2978121a542595cfc9
Ibrahim-Hegazi <115431559+Ibrahim-Hegazi@users.noreply.github.com>
2023-12-17 19:35:55 +0200

    Create A. Word Capitalization (281 A).cpp

---
+++ A. Word Capitalization (281 A).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define ld long 
+#define ull unsigned long long
+using namespace std;
+
+int main() {
+    fast;
+  
+    string s; cin >> s;
+    s[0]=toupper(s[0]);
+    cout << s;
+
+  return 0;
+}
