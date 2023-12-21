1fa0540fb86f4bd7c59b9a50c8a5a94d1f829fe0
Ibrahim-Hegazi <115431559+Ibrahim-Hegazi@users.noreply.github.com>
2023-12-21 21:35:36 +0200

    Create A. Wrong Subtraction (977 A).cpp

---
+++ A. Wrong Subtraction (977 A).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define ld long 
+#define ull unsigned long long
+#define loop(n) for(ll i=0;i<n;i++)
+#define lop(m) for(ll j=0;j<m:j++)
+#define lpse(i,s,e)for (ll i = s; i <=e ; i++)
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    while (k--) {
+        if (n % 10 != 0) {
+            n--;
+        }
+        else {
+            n /= 10;
+        }
+    }
+    cout << n;
+    return 0;
+}
