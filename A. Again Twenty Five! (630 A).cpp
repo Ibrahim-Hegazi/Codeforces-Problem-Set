b192b29a32708e219bd4cd631e633cafe8dfa313
Ibrahim-Hegazi <115431559+Ibrahim-Hegazi@users.noreply.github.com>
2024-01-04 18:31:09 +0200

    Create A. Again Twenty Five! (630 A).cpp

---
+++ A. Again Twenty Five! (630 A).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include<bits/stdc++.h>
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define ld long 
+#define ull unsigned long long
+using namespace std;
+
+int main() {
+    fast;
+    ll n; cin >> n;
+    cout << 25;
+    return 0;
+}
