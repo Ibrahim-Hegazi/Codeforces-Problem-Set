0c4a5d8f4a27948b0afaee80bc02d24463749384
Ibrahim-Hegazi <115431559+Ibrahim-Hegazi@users.noreply.github.com>
2024-01-14 20:51:52 +0200

    Create A. Marathon (1692 A).cpp

---
+++ A. Marathon (1692 A).cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#include <string.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define ll long long
+#define srt(v) sort(v.begin(),v.end());
+#define rsrt(v) sort(v.rbegin(),v.rend());
+#define pb push_back
+#define vll vector<ll>
+#define vp vector<pair<ll,ll>>
+
+
+
+void solve() {
+    //int n; cin >> n;
+    int a, b, c, d,cnt{0};
+    cin >> a >> b >> c >> d;
+    if (b > a)
+        cnt++;
+    if (c > a)
+        cnt++;
+    if (d > a)cnt++;
+    cout << cnt << "\n";
+}
+
+int main() {
+    fast;
+    
+    int t{1}; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
