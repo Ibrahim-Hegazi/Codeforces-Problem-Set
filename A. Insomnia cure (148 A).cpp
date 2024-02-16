beeba6498c5af0d79c9ac259c20cef08ac3b52aa
Ibrahim-Hegazi <115431559+Ibrahim-Hegazi@users.noreply.github.com>
2024-02-17 00:28:31 +0200

    Create A. Insomnia cure (148 A).cpp

---
+++ A. Insomnia cure (148 A).cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int k, l, m, n, d;
+	cin >> k >> l >> m >> n >> d;
+
+	set<int> count;
+	for (int i = k; i <= d; i += k)
+		count.insert(i);
+	for (int i = l; i <= d; i += l)
+		count.insert(i);
+	for (int i = m; i <= d; i += m)
+		count.insert(i);
+	for (int i = n; i <= d; i += n)
+		count.insert(i);
+
+	cout << count.size() << endl;
+}
