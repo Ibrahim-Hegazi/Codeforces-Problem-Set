91f266ac379c87206f744d66f3b4e1ba339dc271
Ibrahim-Hegazi <115431559+Ibrahim-Hegazi@users.noreply.github.com>
2024-03-31 21:37:54 +0200

    Create A. Candies and Two Sisters (1335 A).cpp

---
+++ A. Candies and Two Sisters (1335 A).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+const ll MAXNODES = (ll)1e10;
+
+
+
+void solve() {
+    ll n; cin >> n;
+    cout << (n - 1)/2 <<"\n";
+}
+
+int main() {
+    int t{ 1 }; cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
